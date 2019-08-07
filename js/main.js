f97839ae34fc7d93e13c98c797e4f7e42ba78a59
khaled311 <k.sarhan311@gmail.com>
2019-08-07 15:49:38 +0200

    Deploy

---
+++ js/main.js
@@ -0,0 +1,81 @@
+$(function() {
+
+  // Toggle open
+  $(".sideNav li").on("click", function(e) {
+    $(this).find(".drop").slideToggle();
+    $(this).siblings().find(".drop").slideUp();
+    $(this).find("i").toggleClass("open");
+    $(this).siblings().find("i").removeClass("open");
+    e.stopPropagation();
+});
+
+
+  // open Side Nav
+  $(".menuTriger").on("click", function() {
+      $(this).fadeOut();
+      $(".sideNav").toggleClass("open");
+      $(".navover").toggleClass("open");
+      $("body").css("overflow", "hidden");
+  });
+
+
+  // Close Side Nav
+  $(".navover, .close1").on("click", function() {
+      if($(".sideNav").hasClass("open")){
+          $(".menuTriger").fadeIn();
+          $(".navover").removeClass("open");
+          $(".sideNav").toggleClass("open");
+          $("body").css("overflow", "auto");
+      }
+  });
+
+  // Main Slider
+  $('.main-slider .owl-carousel').owlCarousel({
+    autoplay: true,
+    rtl:true,
+    loop:true,
+    nav:true,
+    items: 1,
+    dots: true,
+    smartSpeed: 1000,
+    navText: ["<span></span>","<span></span>"]
+  });
+
+  // clients Slider
+  $('.clients .owl-carousel').owlCarousel({
+    // autoplay: true,
+    rtl:true,
+    loop:true,
+    nav:true,
+    items: 7,
+    dots: true,
+    smartSpeed: 1000,
+    responsive:{
+      0:{
+          items:1
+      },
+      768:{
+          items:3
+      },
+      992:{
+          items:5
+      },
+      1200:{
+          items:7
+      }
+    },
+    navText: ["<span></span>","<span></span>"]
+  });
+
+  //footer accordion
+  if($(window).width() < 992){
+    $(".foot-links button").attr("data-toggle", "collapse");
+  }
+
+  $('.foot-links button').on('click', function() {
+      $(this).find("i").toggleClass("trans");
+      $(this).parent().parent().siblings().find('.collapse').collapse('hide');
+      $(this).parent().parent().siblings().find('button i').removeClass("trans");
+  });
+
+})
